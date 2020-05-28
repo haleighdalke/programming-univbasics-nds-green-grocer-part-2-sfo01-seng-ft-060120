b8c8f64c1038af758c0732d531b83ef598e3b9f7
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-28 18:38:45 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -27,11 +27,15 @@ end
 
 cart_test = [
   {:item => "AVOCADO", :price => 3.00, :clearance => true},
+  {:item => "AVOCADO", :price => 3.00, :clearance => true},
+  {:item => "AVOCADO", :price => 3.00, :clearance => true},
   {:item => "KALE", :price => 3.00, :clearance => false},
   {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
   {:item => "ALMONDS", :price => 9.00, :clearance => false},
   {:item => "TEMPEH", :price => 3.00, :clearance => true},
   {:item => "CHEESE", :price => 6.50, :clearance => false},
+  {:item => "CHEESE", :price => 6.50, :clearance => false},
+  {:item => "BEER", :price => 13.00, :clearance => false},
   {:item => "BEER", :price => 13.00, :clearance => false},
   {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
   {:item => "BEETS", :price => 2.50, :clearance => false},
