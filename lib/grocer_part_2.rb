195350941a6e1b3c4c0776b765411c60fe532dc7
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-28 18:48:30 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -3,28 +3,9 @@ require 'pry'
 
 def apply_coupons(cart, coupons)
 
-  cart.each do |grocery_item|
-    coupons.each do |coupon_item|
-      if grocery_item[:item] == coupon_item[:item] && grocery_item[:clearance] == true
-
-        discount_item = {
-          :item => "#{grocery_item[:item].upcase} W/COUPON",
-          :price => coupon_item[:cost] / coupon_item[:num],
-          :clearance => true,
-          :count => coupon_item[:num]
-        }
-        grocery_item[:count] -= coupon_item[:num]
-
-        cart.push(discount_item)
-
-        binding.pry
-      end
-    end
-  end
-  binding.pry
-  cart
 end
 
+# ------- my tests --------
 cart_test = [
   {:item => "AVOCADO", :price => 3.00, :clearance => true},
   {:item => "AVOCADO", :price => 3.00, :clearance => true},
@@ -51,7 +32,7 @@ coupons_test = [
 cart_test = consolidate_cart(cart_test)
 apply_coupons(cart_test, coupons_test)
 
-
+# -------------------------------
 
 
 
@@ -76,3 +57,29 @@ def checkout(cart, coupons)
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
 end
+
+
+
+
+  #
+  #
+  # cart.each do |grocery_item|
+  #   coupons.each do |coupon_item|
+  #     if grocery_item[:item] == coupon_item[:item] && grocery_item[:clearance] == true
+  #
+  #       discount_item = {
+  #         :item => "#{grocery_item[:item].upcase} W/COUPON",
+  #         :price => coupon_item[:cost] / coupon_item[:num],
+  #         :clearance => true,
+  #         :count => coupon_item[:num]
+  #       }
+  #       grocery_item[:count] -= coupon_item[:num]
+  #
+  #       cart.push(discount_item)
+  #
+  #       binding.pry
+  #     end
+  #   end
+  # end
+  # binding.pry
+  # cart
