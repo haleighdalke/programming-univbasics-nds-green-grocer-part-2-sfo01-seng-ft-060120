d3d6f83a7fa8eb8eae8aefb52ccfb275550e63dc
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-28 20:10:10 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -63,7 +63,7 @@ def apply_clearance(cart)
   #
   # REMEMBER: This method **should** update cart
   cart.each do |item|
-    if item[:clearance] == true
+    if item[:clearance]
       item[:price] = item[:price] - item[:price] * 0.2
     end
   end
